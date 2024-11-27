214f4866d3b7551b3b1c7c64b18f4852f08224e2
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2024-11-27 23:36:16 +0100

    make my responsive

--- assets/player.js
+++ assets/player.js
@@ -1,3 +1,4 @@
+// --------------------------fetch JSON file----------------------------------------//
 document.addEventListener("DOMContentLoaded", () => {
     fetch('../data.json')
         .then(response => response.json())
@@ -12,11 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
                     <img src="${player.photo}" alt="${player.name}">
                     <h3>${player.name}</h3>
                     <p>${player.position}</p>
-                     <p>$ ${player.club}</p>
+                    <p>${player.club}</p>
                     <p>${player.rating}</p>
                 `;
                 grid.appendChild(card);
             });
-            console.log("Players:", players);
-        })  
+        })
+        .catch(error => console.error("Erreur lors du chargement des joueurs :", error)); 
 });
+
+// ----------------------------show and hide popup-----------------//
+const addButton = document.getElementById("add1"); 
+const cancelButton = document.getElementById("cancelPlayer");
+function togglePopup() {
+    const pop = document.getElementById("footballPopupform");
+    pop.classList.toggle("hidden");
+}
+addButton.addEventListener("click", togglePopup);
+cancelButton.addEventListener("click", togglePopup);
+// -----------------------------------------------------------------//
